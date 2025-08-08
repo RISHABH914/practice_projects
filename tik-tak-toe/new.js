ff1b17a7315e6703ffb3e8f87ed79a358e126b1a
RISHABH914 <rishabh.goyal@students.iiit.ac.in>
2025-08-08 06:52:30 +0530

    gello

--- tik-tak-toe/new.js
+++ tik-tak-toe/new.js
@@ -12,7 +12,7 @@ function join(){
     const room=document.getElementById("room").value;
     const username=document.getElementById("username").value;
     alert(`${room} joined`);
-    ws=new WebSocket(`wss://practice-projects-jlk1.onrender.com/ws/${room}/${username}`);
+    ws=new WebSocket(`ws://practice-projects-jlk1.onrender.com/ws/${room}/${username}`);
 
     ws.onmessage = (event) => {
         const data=JSON.parse(event.data);
